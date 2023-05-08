e57f99e7b01082199a33f9e50f0f7ecfad980e9b
cable <CMaclean98@gmail.com>
2023-05-08 12:03:33 -0700

    Finished All Functions

--- script.js
+++ script.js
@@ -47,12 +47,133 @@ function addKeyAndValue(arr,key,value) {
     })
     return result;
 };
+// console.log(addKeyAndValue([
+//     {name: 'Elie'},
+//     {name: 'Tim'},
+//     {name: 'Matt'},
+//     {name: 'Colt'}
+//   ],
+//     'title',
+//     'instructor'));
 
-console.log(addKeyAndValue([
-    {name: 'Elie'},
-    {name: 'Tim'},
-    {name: 'Matt'},
-    {name: 'Colt'}
-  ],
-    'title',
-    'instructor'));
+// Vowel Count Function
+function vowelCount(str) {
+    const resultObj = {};
+    const vowels = 'aeiou';
+    const strVowelArr = [];
+
+    // For loop to fill strVowelArr with the vowels of str
+    for(let letter of str) {
+        // Making the function case insensitive
+        const char = letter.toLowerCase();
+        // For loop to check char against vowels
+        for(let vowel of vowels) {
+            if(char === vowel) {
+                strVowelArr.push(char);
+            }
+        }
+    }
+
+    strVowelArr.forEach(vowel => {
+        if(resultObj[vowel]) {
+            resultObj[vowel] += 1;
+        } else {
+            resultObj[vowel] = 1;
+        }
+    })
+    return resultObj;
+};
+
+// Map Section
+// Double Values With Map Function
+function doubleValuesWithMap(arr) {
+    return arr.map(num => {
+        return num * 2;
+    })
+};
+
+// Value Times Index Function
+function valTimesIndex(arr) {
+    let index = 0;
+    return arr.map(value => {
+        let result = value * index;
+        index++;
+        return result;
+    })
+};
+
+// Extract Key Function
+function extractKey(arr, key) {
+    return arr.map(obj => {
+        return obj[key];
+    })
+};
+
+// Extract Full Name Function
+function extractFullName(arr) {
+    return arr.map(obj => {
+        return obj['first'] + ' ' + obj['last'];
+    })
+};
+
+// Filter Section
+// Filter by Value Function
+function filterByValue(arr, key) {
+    return arr.filter(obj => {
+        return obj[key];
+    })
+};
+
+// Find Function
+function find(arr, val) {
+    const findArr = arr.filter(element => {return element === val});
+    if(findArr[0]) {
+        return findArr[0];
+    } else {
+        return undefined;
+    }
+};
+
+// Find in Object Function
+function findInObj(arr, key, val) {
+    const findInObjArr = arr.filter(obj => {return obj[key] === val});
+    if(findInObjArr[0]) {
+        return findInObjArr[0];
+    } else {
+        return undefined;
+    }
+};
+
+// Remove Vowels Function
+function removeVowels(str) {
+    const vowels = 'aeiou';
+    const strArr = Array.from(str);
+    const noVowelArr = strArr.filter(char => {
+        let letter = char.toLowerCase();
+        switch(letter) {
+            case 'a':
+                return false;
+                break;
+            case 'i':
+                return false;
+                break;
+            case 'e':
+                return false;
+                break;
+            case 'o':
+                return false;
+                break;
+            case 'u':
+                return false;
+                break;
+            default:
+                return true;
+        }
+    });
+    return noVowelArr.join("");
+};
+
+// Double Odd Numbers Function
+function doubleOddNumbers(arr) {
+    return arr.filter(num => {return num % 2 !== 0}).map(num => {return num * 2});
+};
